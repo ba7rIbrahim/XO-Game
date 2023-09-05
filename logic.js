343f2a494f62265e4cdfde2857bd52b0084aa39a
BakrAli <ilbakr2@gmail.com>
2023-09-05 16:43:07 +0300

    Upload

---
+++ logic.js
@@ -0,0 +1,116 @@
+let items = document.querySelectorAll('.board div');
+let playerTurn = document.querySelector('.player-turn span');
+let board = document.querySelector('.board');
+let resetButton = document.querySelector('.reset');
+let textCount = 0;
+let gameIsFinish = false;
+let currentTurn = 'x';
+let xWin = document.querySelector('.num-of-win .x');
+let oWin = document.querySelector('.num-of-win .o');
+
+// Check Session Storage
+if(sessionStorage.getItem('xWinner')) {
+  xWin.innerHTML = sessionStorage.getItem('xWinner');
+}
+if(sessionStorage.getItem('oWinner')) {
+  oWin.innerHTML = sessionStorage.getItem('oWinner');
+}
+
+items.forEach((ele) => {
+  ele.onclick = () => {
+    if(textCount < items.length) {
+      if(ele.innerHTML === '') {
+        ele.innerHTML = currentTurn;
+        textCount++;
+
+        if(currentTurn === 'x') {
+          playerTurn.innerHTML = currentTurn = 'o';
+          
+        }
+        else if(currentTurn === 'o') {
+          playerTurn.innerHTML = currentTurn = 'x';
+        }
+      }
+
+      evaluateBoard(items);
+      
+    }
+  }
+});
+
+resetButton.onclick = () => {
+  window.location.reload();
+}
+
+
+function evaluateBoard(items) {
+  if(
+    // Columns
+    (items[0].innerHTML === 'x' && items[1].innerHTML === 'x' && items[2].innerHTML === 'x') ||
+    (items[0].innerHTML === 'o' && items[1].innerHTML === 'o' && items[2].innerHTML === 'o') ||
+    (items[3].innerHTML === 'x' && items[4].innerHTML === 'x' && items[5].innerHTML === 'x') ||
+    (items[3].innerHTML === 'o' && items[4].innerHTML === 'o' && items[5].innerHTML === 'o') ||
+    (items[6].innerHTML === 'x' && items[7].innerHTML === 'x' && items[8].innerHTML === 'x') ||
+    (items[6].innerHTML === 'o' && items[7].innerHTML === 'o' && items[8].innerHTML === 'o') ||
+
+    //Rows
+    (items[0].innerHTML === 'x' && items[3].innerHTML === 'x' && items[6].innerHTML === 'x') ||
+    (items[0].innerHTML === 'o' && items[3].innerHTML === 'o' && items[6].innerHTML === 'o') ||
+    (items[1].innerHTML === 'x' && items[4].innerHTML === 'x' && items[7].innerHTML === 'x') ||
+    (items[1].innerHTML === 'o' && items[4].innerHTML === 'o' && items[7].innerHTML === 'o') ||
+    (items[2].innerHTML === 'x' && items[5].innerHTML === 'x' && items[8].innerHTML === 'x') ||
+    (items[2].innerHTML === 'o' && items[5].innerHTML === 'o' && items[8].innerHTML === 'o') ||
+
+    // Diagonal
+    (items[0].innerHTML === 'x' && items[4].innerHTML === 'x' && items[8].innerHTML === 'x') ||
+    (items[0].innerHTML === 'o' && items[4].innerHTML === 'o' && items[8].innerHTML === 'o') ||
+    (items[2].innerHTML === 'x' && items[4].innerHTML === 'x' && items[6].innerHTML === 'x') ||
+    (items[2].innerHTML === 'o' && items[4].innerHTML === 'o' && items[6].innerHTML === 'o')
+  ) {
+    let winner = currentTurn === 'o' ? 'x' : 'o';
+    showResult(winner);
+  }
+
+  // Check For Draw..
+  let isDraw = true;
+  items.forEach((ele) => {
+    if(ele.innerHTML != 'x' && ele.innerHTML != 'o') {
+      isDraw = false;
+    }
+  });
+
+  if(isDraw) {
+    var winner = 'Draw!!';
+    showResult(winner);
+  }
+}
+
+function showResult(winner) {
+  board.style.display = 'none';
+  let resultDiv = document.querySelector('.result');
+  resultDiv.innerHTML = `" ${winner} " ${winner !== 'Draw!!' ? 'Is Win!!' : ''}`;
+  resultDiv.style.cssText = `
+  background-color: #FFC107;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  font-size: 30px;
+  padding: 10px;
+  height: calc(250px - 1px);
+  margin: 15px 0;
+  border-radius: 12px;
+  `
+  storageValue(winner);
+}
+
+function storageValue(winner) {
+  if(winner === 'x') {
+    xWin.innerHTML++;
+    sessionStorage.setItem('xWinner', xWin.innerHTML);
+  }
+  else {
+    oWin.innerHTML++;
+    sessionStorage.setItem('oWinner', oWin.innerHTML);
+  }
+}
