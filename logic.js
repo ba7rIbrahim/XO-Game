cb26d29190a7bdee894c636ed6a9a8a60bb153df
BakrAli <ilbakr2@gmail.com>
2023-09-12 13:48:10 +0300

    Edit File

--- logic.js
+++ logic.js
@@ -1,94 +1,87 @@
-let items = document.querySelectorAll('.board div');
-let playerTurn = document.querySelector('.player-turn span');
-let board = document.querySelector('.board');
-let resetButton = document.querySelector('.reset');
-let textCount = 0;
-let gameIsFinish = false;
-let currentTurn = 'x';
-let xWin = document.querySelector('.num-of-win .x');
-let oWin = document.querySelector('.num-of-win .o');
+let items = document.querySelectorAll(".board div");
+let playerTurn = document.querySelector(".player-turn span");
+let board = document.querySelector(".board");
+let resetButton = document.querySelector(".reset");
+let xWin = document.querySelector(".num-of-win .x");
+let oWin = document.querySelector(".num-of-win .o");
+
+let currentTurn = "x";
 
 // Check Session Storage
-if(sessionStorage.getItem('xWinner')) {
-  xWin.innerHTML = sessionStorage.getItem('xWinner');
+if (sessionStorage.getItem("xWinner")) {
+  xWin.innerHTML = sessionStorage.getItem("xWinner");
 }
-if(sessionStorage.getItem('oWinner')) {
-  oWin.innerHTML = sessionStorage.getItem('oWinner');
+if (sessionStorage.getItem("oWinner")) {
+  oWin.innerHTML = sessionStorage.getItem("oWinner");
 }
 
-items.forEach((ele) => {
+items.forEach((ele, index) => { 
   ele.onclick = () => {
-    if(textCount < items.length) {
-      if(ele.innerHTML === '') {
-        ele.innerHTML = currentTurn;
-        textCount++;
+    if(ele.innerHTML === '') {
+      ele.innerHTML = currentTurn;
 
-        if(currentTurn === 'x') {
-          playerTurn.innerHTML = currentTurn = 'o';
-          
-        }
-        else if(currentTurn === 'o') {
-          playerTurn.innerHTML = currentTurn = 'x';
-        }
+      if(currentTurn === 'x') {
+        playerTurn.innerHTML = currentTurn = 'o';
+        
       }
-
-      evaluateBoard(items);
-      
+      else if(currentTurn === 'o') {
+        playerTurn.innerHTML = currentTurn = 'x';
+      }
     }
-  }
+    evaluateBoard(items);
+    };
 });
+
 
 resetButton.onclick = () => {
   window.location.reload();
-}
-
+};
 
 function evaluateBoard(items) {
-  if(
+  if (
     // Columns
-    (items[0].innerHTML === 'x' && items[1].innerHTML === 'x' && items[2].innerHTML === 'x') ||
-    (items[0].innerHTML === 'o' && items[1].innerHTML === 'o' && items[2].innerHTML === 'o') ||
-    (items[3].innerHTML === 'x' && items[4].innerHTML === 'x' && items[5].innerHTML === 'x') ||
-    (items[3].innerHTML === 'o' && items[4].innerHTML === 'o' && items[5].innerHTML === 'o') ||
-    (items[6].innerHTML === 'x' && items[7].innerHTML === 'x' && items[8].innerHTML === 'x') ||
-    (items[6].innerHTML === 'o' && items[7].innerHTML === 'o' && items[8].innerHTML === 'o') ||
-
+    (items[0].innerHTML === "x" && items[1].innerHTML === "x" && items[2].innerHTML === "x") ||
+    (items[0].innerHTML === "o" && items[1].innerHTML === "o" && items[2].innerHTML === "o") ||
+    (items[3].innerHTML === "x" && items[4].innerHTML === "x" && items[5].innerHTML === "x") ||
+    (items[3].innerHTML === "o" && items[4].innerHTML === "o" && items[5].innerHTML === "o") ||
+    (items[6].innerHTML === "x" && items[7].innerHTML === "x" && items[8].innerHTML === "x") ||
+    (items[6].innerHTML === "o" && items[7].innerHTML === "o" && items[8].innerHTML === "o") ||
     //Rows
-    (items[0].innerHTML === 'x' && items[3].innerHTML === 'x' && items[6].innerHTML === 'x') ||
-    (items[0].innerHTML === 'o' && items[3].innerHTML === 'o' && items[6].innerHTML === 'o') ||
-    (items[1].innerHTML === 'x' && items[4].innerHTML === 'x' && items[7].innerHTML === 'x') ||
-    (items[1].innerHTML === 'o' && items[4].innerHTML === 'o' && items[7].innerHTML === 'o') ||
-    (items[2].innerHTML === 'x' && items[5].innerHTML === 'x' && items[8].innerHTML === 'x') ||
-    (items[2].innerHTML === 'o' && items[5].innerHTML === 'o' && items[8].innerHTML === 'o') ||
-
+    (items[0].innerHTML === "x" && items[3].innerHTML === "x" && items[6].innerHTML === "x") ||
+    (items[0].innerHTML === "o" && items[3].innerHTML === "o" && items[6].innerHTML === "o") ||
+    (items[1].innerHTML === "x" && items[4].innerHTML === "x" && items[7].innerHTML === "x") ||
+    (items[1].innerHTML === "o" && items[4].innerHTML === "o" && items[7].innerHTML === "o") ||
+    (items[2].innerHTML === "x" && items[5].innerHTML === "x" && items[8].innerHTML === "x") ||
+    (items[2].innerHTML === "o" && items[5].innerHTML === "o" && items[8].innerHTML === "o") ||
     // Diagonal
-    (items[0].innerHTML === 'x' && items[4].innerHTML === 'x' && items[8].innerHTML === 'x') ||
-    (items[0].innerHTML === 'o' && items[4].innerHTML === 'o' && items[8].innerHTML === 'o') ||
-    (items[2].innerHTML === 'x' && items[4].innerHTML === 'x' && items[6].innerHTML === 'x') ||
-    (items[2].innerHTML === 'o' && items[4].innerHTML === 'o' && items[6].innerHTML === 'o')
+    (items[0].innerHTML === "x" && items[4].innerHTML === "x" && items[8].innerHTML === "x") ||
+    (items[0].innerHTML === "o" && items[4].innerHTML === "o" && items[8].innerHTML === "o") ||
+    (items[2].innerHTML === "x" && items[4].innerHTML === "x" && items[6].innerHTML === "x") ||
+    (items[2].innerHTML === "o" && items[4].innerHTML === "o" && items[6].innerHTML === "o")
   ) {
-    let winner = currentTurn === 'o' ? 'x' : 'o';
+    let winner = currentTurn === "o" ? "x" : "o";
     showResult(winner);
+    return;
   }
-
+  
   // Check For Draw..
-  let isDraw = true;
+  isDraw = true;
   items.forEach((ele) => {
     if(ele.innerHTML != 'x' && ele.innerHTML != 'o') {
-      isDraw = false;
-    }
+      isDraw = false
+    };
   });
 
-  if(isDraw) {
-    var winner = 'Draw!!';
+  if (isDraw) {
+    let winner = "Draw!!";
     showResult(winner);
   }
 }
 
 function showResult(winner) {
-  board.style.display = 'none';
-  let resultDiv = document.querySelector('.result');
-  resultDiv.innerHTML = `" ${winner} " ${winner !== 'Draw!!' ? 'Is Win!!' : ''}`;
+  board.style.display = "none";
+  let resultDiv = document.querySelector(".result");
+  resultDiv.innerHTML = `" ${winner} " ${winner !== "Draw!!" ? "Is Win!!" : ""}`;
   resultDiv.style.cssText = `
   background-color: #FFC107;
   display: flex;
@@ -100,17 +93,52 @@ function showResult(winner) {
   height: calc(250px - 1px);
   margin: 15px 0;
   border-radius: 12px;
-  `
+  `;
   storageValue(winner);
 }
 
 function storageValue(winner) {
-  if(winner === 'x') {
+  if (winner === "x") {
     xWin.innerHTML++;
-    sessionStorage.setItem('xWinner', xWin.innerHTML);
-  }
-  else {
+    sessionStorage.setItem("xWinner", xWin.innerHTML);
+  } else if(winner === "o") {
     oWin.innerHTML++;
-    sessionStorage.setItem('oWinner', oWin.innerHTML);
+    sessionStorage.setItem("oWinner", oWin.innerHTML);
+  } else {
+    xWin.innerHTML;
+    sessionStorage.setItem("xWinner", xWin.innerHTML);
+    oWin.innerHTML;
+    sessionStorage.setItem("oWinner", oWin.innerHTML);
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+// function computerPlayer() {
+//   let randomNumber;
+//   let arr = [];
+//   boardArray.forEach((ele, index) => {
+//     if(ele != 'x' && ele != 'o') {
+//       arr.push(+ele);
+//     }
+//   });
+//   randomNumber = Math.floor(Math.random() * arr.length);
+
+//   items.forEach((ele, index) => {
+//       if(ele.innerHTML === "") {
+//         items[randomNumber].innerHTML = 'o';
+//         boardArray[randomNumber] = 'o';
+//         textCount++;
+//         playerTurn.innerHTML = currentTurn = 'x';
+//       }
+//       evaluateBoard(items);
+//   });
+// }
